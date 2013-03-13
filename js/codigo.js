b512c28e0cee07e6cf3bd481afb83d8b20da2836
Jaime Blandón <blandon.jaime@gmail.com>
2013-03-13 08:51:42 -0600

    Incluyendo url

--- js/codigo.js
+++ js/codigo.js
@@ -1,8 +1,25 @@
+var instituciones = new Array();
+instituciones['ANDA']='http://www.anda.gob.sv/index.php?option=com_content&view=category&layout=blog&id=25&Itemid=76&format=feed&type=rss';
+instituciones['ISSS']='http://www.isss.gob.sv/index.php?option=com_content&view=section&layout=blog&id=4&Itemid=76&format=feed&type=rss';
+instituciones['MAG']='http://www.mag.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['MOP']='http://www.mop.gob.sv/index.php?option=com_content&view=category&id=25&Itemid=76&format=feed&type=rss';
+
 $(document).on('ready',inicio);
 
 function inicio(){
 
-	$("#feedSection").rssfeed('http://www.anda.gob.sv/index.php?option=com_content&view=category&layout=blog&id=25&Itemid=76&format=feed&type=rss',
-		{limit:5});
+	$('#instituciones').on('change',cargarRss);
 	
+}
+
+function cargarRss()
+{
+
+	var rssId = $('#instituciones option:selected').val();
+	var rssUrl =  instituciones[rssId];
+
+	$("#feedSection").html('Cargando ...');
+	$("#feedSection").rssfeed(rssUrl,
+		{limit:5});
+
 }
