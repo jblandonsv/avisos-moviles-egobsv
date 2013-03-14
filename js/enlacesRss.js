62e460c807fa51eac379b98ecf2aa74a6cffbfbc
Jaime Blandón <blandon.jaime@gmail.com>
2013-03-13 22:17:19 -0600

    Varios Cambios
    * Se agrupan las opciones por ministerios y autonomas
    * Se incorporó el ruteo en el archivo enlacesRSS.js

--- js/enlacesRss.js
+++ js/enlacesRss.js
@@ -1,8 +1,40 @@
 var instituciones = new Array();
 instituciones['ASAMBLEA']='http://www.asamblea.gob.sv/noticias/rss?com=1&Subject:list=Econom%C3%ADa%20y%20agricultura';
-instituciones['ANDA']='http://www.anda.gob.sv/index.php?option=com_content&view=category&layout=blog&id=25&Itemid=76&format=feed&type=rss';
-instituciones['ISSS']='http://www.isss.gob.sv/index.php?option=com_content&view=section&layout=blog&id=4&Itemid=76&format=feed&type=rss';
+
+/*MINISTERIOS*/
+instituciones['DEFENSA']='';
+instituciones['GOBERNACION']='http://www.gobernacion.gob.sv/index.php?option=com_content&view=category&id=25&Itemid=76&format=feed&type=rss';
 instituciones['MAG']='http://www.mag.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['MARN'] = 'http://www.marn.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['MINEC'] = 'http://www.minec.gob.sv/index.php?option=com_content&view=category&id=51&Itemid=76&format=feed&type=rss';
+instituciones['MINED'] = '';
+instituciones['MINTRAB'] = 'http://www.mtps.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['MITUR']='';
 instituciones['MOP']='http://www.mop.gob.sv/index.php?option=com_content&view=category&id=25&Itemid=76&format=feed&type=rss';
 instituciones['PNC']='http://www.pnc.gob.sv/core/index.php/component/content/category/51?format=feed&type=rss';
+instituciones['RREE']='';
+instituciones['SALUD']='http://www.salud.gob.sv/novedades/noticias/noticias-ciudadanosas.feed?type=rss';
+instituciones['SEGURIDAD'] = '';
 instituciones['VMT']='http://www.vmt.gob.sv/index.php?option=com_content&view=category&id=25&Itemid=76&format=feed&type=rss';
+
+/*AUTONOMAS*/
+instituciones['ANSP']='http://www.ansp.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['ANDA']='http://www.anda.gob.sv/index.php?option=com_content&view=category&layout=blog&id=25&Itemid=76&format=feed&type=rss';
+instituciones['CIFCO']='http://www.cifco.gob.sv/index.php?option=com_k2&view=itemlist&layout=category&task=category&id=3&Itemid=208&format=feed';
+instituciones['CENTA']='http://www.centa.gob.sv/index.php?option=com_content&view=category&id=25&Itemid=76&format=feed&type=rss';
+instituciones['CNR']='http://www.cnr.gob.sv/index.php?option=com_content&view=category&id=1&Itemid=77&format=feed&type=rss';
+instituciones['CEL']='http://www.cel.gob.sv/index.php?option=com_content&view=category&id=25&Itemid=218&format=feed&type=rss';
+instituciones['CEPA']='';
+instituciones['CONAMYPE']='http://www.conamype.gob.sv/index.php/novedades/avisos?format=feed&type=rss';
+instituciones['CNE']='http://www.cne.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['CONJUVE']='';
+instituciones['CSSP']='http://www.cssp.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['DEFENSORIA']='http://www.defensoria.gob.sv/index.php?option=com_content&view=category&layout=blog&id=25&Itemid=76&format=feed&type=rss';
+instituciones['FOVIAL']='http://www.fovial.com/index.php?option=com_content&view=category&layout=blog&id=25&Itemid=76&format=feed&type=rss';
+instituciones['FOMILENIO']='';
+instituciones['FONAVIPO']='';
+instituciones['FOSEP']='http://www.fosep.gob.sv/especial/index.php?option=com_content&view=category&id=25&Itemid=76&format=feed&type=rss';
+instituciones['FSV']='http://www.fsv.gob.sv/index.php?option=com_content&view=category&id=25%3Aavisos-ciudadano&Itemid=76&layout=default&format=feed&type=rss';
+instituciones['ISSS']='http://www.isss.gob.sv/index.php?option=com_content&view=section&layout=blog&id=4&Itemid=76&format=feed&type=rss';
+instituciones['RNPN']='http://www.rnpn.gob.sv/index.php?option=com_content&view=category&id=1%3Anoticias-ciudadano&Itemid=77&layout=default&format=feed&type=rss';
+
