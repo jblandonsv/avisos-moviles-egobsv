984db0645d68dec2b29f9dbca69216171f2ab1c7
Jaime Blandón <blandon.jaime@gmail.com>
2013-03-16 13:12:32 -0600

    Añadiendo Opción para elegir cantidad de Feeds

--- js/codigo.js
+++ js/codigo.js
@@ -2,7 +2,7 @@ $(document).on('ready',inicio);
 
 function inicio(){
 
-	$('#instituciones').on('change',cargarRss);
+	$('#buscar_button').on('click',cargarRss);
 	
 }
 
@@ -10,13 +10,21 @@ function cargarRss()
 {
 
 	var rssId = $('#instituciones option:selected').val();
+	//Cantidad de Feeds
+	var limiteFeeds = $('#cantidad_feeds').val();
+	limiteFeeds = parseInt(limiteFeeds);
+	//Ordenamiento por fecha
+	var opcionOrdenamiento = $('#sortByDate option:selected').val();
+	console.log('orden = ' + opcionOrdenamiento);
 	var rssUrl =  instituciones[rssId];
 
 	if(rssUrl!='')
 	{
 		$("#feedSection").html('Cargando ...');
 		$("#feedSection").rssfeed(rssUrl,
-		{limit:5});
+		{
+			limit:limiteFeeds
+		});
 	}else{
 		$("#feedSection").html('<span class="no-encontrado">No se tiene registrado un recursos RSS en la institución, si eres el encargado del sitio, mándanos un email con el RSS para que lo añadamos o Contribuye con nosotros en nuestro repositorio :D </span>');
 	}
